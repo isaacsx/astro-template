1e0245efde2a8e1438c0bbd25c20af17a5263e75
isaacsx <isaacramosfloes454@gmail.com>
2022-12-20 14:00:07 -0600

    first commit

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Font } from "./Font.astro";
+export { default as Navbar } from "./Navbar.astro";
+export { default as Metas } from "./Metas.astro";
---
+++ src/lib/utils/fonts.ts
@@ -0,0 +1,23 @@
+const userAgents: { name: string; ua: string }[] = [
+  {
+    name: "woff",
+    ua: "Mozilla/5.0 (Windows NT 10.0; Trident/7.0; rv:11.0) like Gecko",
+  },
+  {
+    name: "woff2",
+    ua: 'Mozilla/5.0 (Windows NT 6.3; rv:39.0) Gecko/20100101 Firefox/44.0"',
+  },
+];
+
+export function downloadFontCSS(url: string): Promise<string> {
+  const fontDownloads = Promise.all(
+    userAgents.map((entry) => {
+      return fetch(url, { headers: { "User-Agent": entry.ua } })
+        .then((res) => res.text())
+        .then((t) =>
+          t.replace(/  +/g, "").replace(/\t+/g, "").replace(/\n+/g, "")
+        );
+    })
+  );
+  return fontDownloads.then((contents) => contents.join(" "));
+}
